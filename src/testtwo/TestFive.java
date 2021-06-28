3ef8d2f33ea4eb34681da1c2c2bfadce69862f01
zhaochengqi <middleton22203@gmail.com>
2021-06-28 17:17:20 +0800

    add t3 method

--- src/testtwo/TestFive.java
+++ src/testtwo/TestFive.java
@@ -54,6 +54,13 @@ public class TestFive {
 
     }
 
+    static void t3(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+    }
+
 
 
 }
