49e5aab102253b48871fe446cacde4db2d7facd9
zhaochengqi <middleton22203@gmail.com>
2021-06-29 11:52:31 +0800

    update TestSix

---
+++ src/testtwo/TestSix.java
@@ -0,0 +1,17 @@
+package testtwo;
+
+public class TestSix {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("bbb");
+
+        System.out.println("ccc");
+        System.out.println("ddd");
+        System.out.println("fff");
+        System.out.println("eee");
+
+        System.out.println("ggg");
+
+    }
+
+}
