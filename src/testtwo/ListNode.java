de0ec9e420b37237962ee41f726a527fa4ee6442
zhaochengqi <middleton22203@gmail.com>
2021-07-26 14:28:49 +0800

    update TestSix

--- src/testtwo/ListNode.java
+++ src/testtwo/ListNode.java
@@ -1,10 +1,10 @@
 package testtwo;
 
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
         val = x;
     }
-    ListNode(){}
+    public ListNode(){}
 }
