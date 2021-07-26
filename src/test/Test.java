0fb06c3ba26c56bf745c59fbd1199a3910fdca7e
zhaochengqi <middleton22203@gmail.com>
2021-07-26 14:34:13 +0800

    update test test

---
+++ src/test/Test.java
@@ -0,0 +1,60 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Test {
+    public static void main(String[] args) {
+
+        ReentrantLock reentrantLock = new ReentrantLock();
+        reentrantLock.lock();
+
+        int[] arr = {111, 1, 1, 1};
+        Integer[] arr1 = {1, 1, 1, 1, 1, 1};
+
+        It it = a -> System.out.println(a);
+        Arrays.sort(arr);
+
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+
+        list.forEach((item) -> System.out.println(item));
+
+        list.forEach(item -> {
+            if (1 == item) {
+                System.out.println(true);
+            }
+        });
+
+        list.forEach(item -> {
+            if (item == 1) {
+                System.out.println(true);
+            }
+        });
+
+        list.forEach(item -> {
+            if (item == 10) {
+                System.out.println(false);
+            }
+        });
+
+
+
+    }
+    interface It{
+        public void getX(int[] a);
+    }
+
+
+
+
+
+}
