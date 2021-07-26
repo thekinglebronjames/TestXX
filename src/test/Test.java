7ca5f323d5c8f65712145278b58560a8d237098b
zhaochengqi <middleton22203@gmail.com>
2021-07-26 14:41:55 +0800

    Merge branch 'master' into dveTest
    # Conflicts:
    #	src/test/Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -18,7 +18,7 @@ public class Test {
         Arrays.sort(arr);
 
 
-        List<Integer> list = new ArrayList<>();1
+        List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(2);
