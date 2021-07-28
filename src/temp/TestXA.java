99ce24f3c1e3ffdf1a5f1bd5f20784792ffcb6ac
zhaochengqi <middleton22203@gmail.com>
2021-07-28 20:01:47 +0800

    xa 3

--- src/temp/TestXA.java
+++ src/temp/TestXA.java
@@ -4,5 +4,6 @@ public class TestXA {
     public static void main(String[] args) {
         System.out.println("xa111");
         System.out.println("xa222");
+        System.out.println("xa333");
     }
 }
