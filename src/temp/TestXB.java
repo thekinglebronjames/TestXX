34722cbbb03d8b9f17e5d4b4f5d7a6d9f74e3223
zhaochengqi <middleton22203@gmail.com>
2021-07-28 20:34:23 +0800

    xb 9
    # Conflicts:
    #	src/temp/TestXB.java

--- src/temp/TestXB.java
+++ src/temp/TestXB.java
@@ -11,5 +11,7 @@ public class TestXB {
         System.out.println("xb555");
         System.out.println("xb6666666");
         System.out.println("xb777");
+        System.out.println("xb888");
+        System.out.println("xb999");
     }
 }
