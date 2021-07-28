7bcc1d809c8cb7fe16ceeaba092a8a5bec5e5e1d
zhaochengqi <middleton22203@gmail.com>
2021-07-28 19:41:27 +0800

    test 888

--- src/test/TestTempTwo.java
+++ src/test/TestTempTwo.java
@@ -7,5 +7,6 @@ public class TestTempTwo {
         System.out.println("555");
         System.out.println("666....");
         System.out.println("777....");
+        System.out.println("888");
     }
 }
