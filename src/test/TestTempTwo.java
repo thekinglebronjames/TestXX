4648c3661df5c812e65c05bbac0fbfb3ad53754f
zhaochengqi <middleton22203@gmail.com>
2021-07-28 19:27:58 +0800

    testTemp 555

--- src/test/TestTempTwo.java
+++ src/test/TestTempTwo.java
@@ -4,5 +4,6 @@ public class TestTempTwo {
     public static void main(String[] args) {
         System.out.println("333");
         System.out.println("444");
+        System.out.println("555");
     }
 }
