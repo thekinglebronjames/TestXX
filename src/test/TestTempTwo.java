00732ef2290bb88bd9c23bcfe97725dc7881cf19
zhaochengqi <middleton22203@gmail.com>
2021-07-28 19:37:31 +0800

    testTemp 777
    # Conflicts:
    #	src/test/TestTempTwo.java

--- src/test/TestTempTwo.java
+++ src/test/TestTempTwo.java
@@ -5,5 +5,7 @@ public class TestTempTwo {
         System.out.println("333");
         System.out.println("444");
         System.out.println("555");
+        System.out.println("666");
+        System.out.println("777");
     }
 }
