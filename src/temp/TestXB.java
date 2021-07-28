f5897c2b8b637518806e58ae159cfccd28f9d221
zhaochengqi <middleton22203@gmail.com>
2021-07-28 20:05:41 +0800

    xb 3

--- src/temp/TestXB.java
+++ src/temp/TestXB.java
@@ -4,5 +4,6 @@ public class TestXB {
     public static void main(String[] args) {
         System.out.println("xb111");
         System.out.println("xb222");
+        System.out.println("xb333");
     }
 }
