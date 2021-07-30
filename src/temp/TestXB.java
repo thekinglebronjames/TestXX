05e5c51219c3a48fad47260d32a5214c238a3ceb
zhaochengqi <middleton22203@gmail.com>
2021-07-30 17:49:46 +0800

    xb

--- src/temp/TestXB.java
+++ src/temp/TestXB.java
@@ -4,34 +4,5 @@ package temp;
 
 public class TestXB {
     public static void main(String[] args) {
-        System.out.println("xb111");
-        System.out.println("xb222");
-        System.out.println("xb333");
-        System.out.println("xb444");
-        System.out.println("xb555");
-        System.out.println("xb6666666");
-        System.out.println("xb777");
-        System.out.println("xb888");
-        System.out.println("xb999");
-        System.out.println("xb101010");
-        System.out.println("xb11111");
-        System.out.println("xb1212");
-        System.out.println("xb1313");
-        System.out.println("xb14");
-        System.out.println("xb15");
-        System.out.println("xb16");
-        System.out.println("xb17");
-        System.out.println("xb18");
-        System.out.println("xb1919");
-        System.out.println("xb20");
-        System.out.println("xb21");
-        System.out.println("xb22");
-        System.out.println("xb23");
-        System.out.println("xb24");
-        System.out.println("xb25");
-        System.out.println("xb26");
-        System.out.println("xb27");
-        System.out.println("xb281");
-        System.out.println("xb2911123");
-    }
+  }
 }
