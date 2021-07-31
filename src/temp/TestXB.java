a5187d84d3144fc5b54113239407bceca68b684a
zhaochengqi <middleton22203@gmail.com>
2021-07-31 14:24:49 +0800

    m xb 88

--- src/temp/TestXB.java
+++ src/temp/TestXB.java
@@ -11,6 +11,6 @@ public class TestXB {
         System.out.println("xb5");
         System.out.println("xb6");
         System.out.println("xb77");
-        System.out.println("xb8");
+        System.out.println("xb88");
   }
 }
