6eeca9ce8d5406acd04393fd524edebc21b1fd40
zhaochengqi <middleton22203@gmail.com>
2021-07-31 15:23:28 +0800

    m xa 2

--- src/temp/TestXA.java
+++ src/temp/TestXA.java
@@ -5,5 +5,6 @@ import java.net.SocketTimeoutException;
 public class TestXA {
     public static void main(String[] args) {
         System.out.println("xa1");
+        System.out.println("xa2");
     }
 }
